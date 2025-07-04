9d8946ca320d9aef55831c991e4ed19a0287e770
MAKARA PINN <MAKARA.PINN@pnc.lan>
2025-07-04 18:12:49 +0700

    certificates

---
+++ ex.py
@@ -0,0 +1,19 @@
+import openpyxl
+from docxtpl import DocxTemplate
+filename= 'list.xlsx'
+workbook = openpyxl.load_workbook(filename)
+sheet = workbook.active
+studentlist = list(sheet.values)
+for student in studentlist:
+    print(student)
+
+# get template
+template = DocxTemplate('word.docx')
+for student in studentlist[1:]:
+    template.render({
+        'first_name':student[2],
+        'last_name': student[0],
+        'class':student[1],
+    })
+    doc_name= str(student[0]) + '.docx'
+    template.save(doc_name)
